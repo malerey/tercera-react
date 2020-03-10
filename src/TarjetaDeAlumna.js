05dab7db24ca6a635b4b438b23c95f67a955caf6
Malena Rey <malerey@male.5d640511d691.sn.mynetname.net>
2020-03-09 21:40:44 -0300

    first commit

---
+++ src/TarjetaDeAlumna.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const TarjetaDeAlumna = props => {
+  return(
+    <div>
+      <h1>{props.nombreDeAlumna}</h1>
+      <p>Informacion de la alumna</p>
+    </div>
+  )
+};
+
+export default TarjetaDeAlumna; 
+
